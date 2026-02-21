006fbe7b94739aa81a2282fbbe558ec08472f40e
Coldboltage <alandreid@hotmail.co.uk>
2026-02-21 16:33:12 +0000

    Refactor revalidate API to use productName instead of productId for improved clarity

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -11,23 +11,24 @@ export default async function handler(
   }
 
   const secret = req.query.secret;
-  const productId = req.query.productId;
+  const productName = req.query.productName;
 
   if (typeof secret !== "string" || secret !== process.env.WEBSITE_SECRET) {
     return res.status(401).json({ ok: false, message: "Invalid secret" });
   }
 
-  if (typeof productId !== "string" || productId.length < 10) {
+  if (typeof productName !== "string" || productName.length < 10) {
     return res
       .status(400)
-      .json({ ok: false, message: "Missing/invalid productId" });
+      .json({ ok: false, message: "Missing/invalid productName" });
   }
 
-  const path = `/product/${productId}`;
+  const path = `/product/${productName}`;
+  console.log(`/product/${productName}`);
 
   try {
     await res.revalidate(path);
-    const response = { ok: true, path, productId, ts: Date.now() };
+    const response = { ok: true, path, productName, ts: Date.now() };
     console.log(response);
     return res.status(200).json(response);
   } catch (e) {

db4e720249443f0cbb3d07b7a418448c2ddf8c0a
Coldboltage <alandreid@hotmail.co.uk>
2026-02-27 14:52:52 +0000

    Add TypeScript types for API handler in geo.ts

--- src/pages/api/geo.ts
+++ src/pages/api/geo.ts
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const country = req.headers["x-vercel-ip-country"];
   console.log(country);
   res.status(200).json({ country });
